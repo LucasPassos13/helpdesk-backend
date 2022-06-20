00f3ed8bd32cc8b74979c5c099780a02468dc59b
LucasPassos13 <lucasp.guia@gmail.com>
2022-06-20 14:50:13 -0300

    Carga inicial da base de dados - 2

--- src/main/java/com/lucas/helpdesk/HelpdeskApplication.java
+++ src/main/java/com/lucas/helpdesk/HelpdeskApplication.java
@@ -37,14 +37,25 @@ public class HelpdeskApplication implements CommandLineRunner  {
 		Tecnico tec1 = new Tecnico(null, "Naruto Uzumaki", "79929366040", "naruto@mail.com", "1234");
 		tec1.addPerfil(Perfil.ADMIN);
 		
+		Tecnico tec2 = new Tecnico(null, "Kushina Uzumaki", "79952251050", "kushina@mail.com", "1234");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Tecnico tec3 = new Tecnico(null, "Maito Gai", "18661645034", "gai@mail.com", "1234");
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		
 		Cliente cli1 = new Cliente(null, "Hinata Hyuga", "89898842091", "hinata@mail.com", "1234");
+		Cliente cli2 = new Cliente(null, "Minato Namekazi", "56694189052", "Minato@mail.com", "1234");
+		Cliente cli3 = new Cliente(null, "Kakashi Hatake", "17217118050", "Kakashi@mail.com", "1234");
 		
 		Chamado c1 = new Chamado(null, Prioridade.MEDIA , Status.ANDAMENTO, "Chamado 01", "Primeiro chamado.", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.ALTA , Status.ANDAMENTO, "Chamado 02", "Segundo chamado.", tec2, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.ALTA , Status.ANDAMENTO, "Chamado 03", "Terceito chamado.", tec3, cli3);
 		
 		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
+		tecnicoRepository.saveAll(Arrays.asList(tec1,tec2,tec3));
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2,cli3));
+		chamadoRepository.saveAll(Arrays.asList(c1,c2,c3));
 	}
 
 }
